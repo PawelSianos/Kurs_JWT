c92f65ae6ee389e72fec8eb10607b6f1ed2f1ede
pawel <pawel.sianos@gmail.com>
2024-01-04 21:41:15 +0100

    pierwszy commit

---
+++ src/main/java/com/sianos/pawel/kurs_jwt/KursJwtApplication.java
@@ -0,0 +1,13 @@
+package com.sianos.pawel.kurs_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sianos/pawel/kurs_jwt/KursJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sianos.pawel.kurs_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
